7c364ad000a177a15b49ff0f06c4266ded017ce0
“sonia” <soniachaparro21@gmail.com>
2023-04-08 16:15:16 -0400

    Add cast to endpoint

--- src/movie/api.ts
+++ src/movie/api.ts
@@ -1,26 +1,20 @@
-import { Films, MovieDetailI, Param } from "@/types";
 import axios from "axios";
+import { Films, Param } from "@/types";
 
 export const URL = "https://api.themoviedb.org/3/movie";
 
-export const getMovies = async (param: Param) => {
+export const getMovies = async (param: Param): Promise<Films> => {
   try {
     const response = await axios.get(`${URL}/${param}`, {
-      params: { api_key: process.env.NEXT_PUBLIC_API_KEY, query: param },
+      params: {
+        api_key: process.env.NEXT_PUBLIC_API_KEY,
+        query: param,
+        append_to_response: "credits",
+      },
     });
+
     const { data } = response;
     return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const getMovie = async (id: string) => {
-  try {
-    const response = await axios.get(`${URL}/${id}`, {
-      params: { api_key: process.env.NEXT_PUBLIC_API_KEY, query: id },
-    });
-    return response;
   } catch (error) {
     console.log(error);
   }
