95637afd79564d41781a45e730e051482d2074a5
“sonia” <soniachaparro21@gmail.com>
2023-04-08 16:37:55 -0400

    Add movie component

---
+++ src/movie/components/Movie.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable @next/next/no-img-element */
+import { MovieI } from "@/types";
+import Img from "@/movie/components/Img";
+import Link from "next/link";
+
+interface Props {
+  movie: MovieI;
+}
+
+export default function MovieComponent({ movie }: Props): JSX.Element {
+  return (
+    <div
+      key={movie.id}
+      className="xl:w-80 lg:w-72 md:w-64 sm:w-auto shadow-lg rounded-2xl m-2 p-2"
+    >
+      <Link href={`/${movie.id}`}>
+        <Img
+          className="rounded-2xl w-auto p-2"
+          src={movie.poster_path}
+          alt={movie.title}
+          width={200}
+          height={300}
+        />
+        <h1 className="text-center font-bold my-2">{movie.title}</h1>
+      </Link>
+    </div>
+  );
+}
